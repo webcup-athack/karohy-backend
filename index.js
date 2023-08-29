0b497c6d92a752ce24a20feb2c3364f57ccfd124
c.rabarijohn <c.rabarijohn@fulldigits.com>
2023-08-29 11:45:23 +0300

    Fix : Fix default scheme

--- index.js
+++ index.js
@@ -53,13 +53,17 @@ function modifySwaggerHost(content, host, scheme) {
 
 app.get('/docs', (req, res) => {
   let host = process.env.HOST || req.headers.host;
-  let scheme = [process.env.SCHEME] || ['https'];
+  let scheme =
+    process.env.SCHEME === 'http' || process.env.SCHEME === 'https'
+      ? [process.env.SCHEME]
+      : ['https'];
   if (
     process.env.ENVIRONMENT === ENVIRONMENT_NAMES.LOCAL ||
     host === `localhost:${port}`
   ) {
     (host = `localhost:${port}`), (scheme = ['http']);
   }
+  console.log(scheme);
 
   fs.readFile('./views/swagger.json', 'utf8', (err, data) => {
     if (err) {
