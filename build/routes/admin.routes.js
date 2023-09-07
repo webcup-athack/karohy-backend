89e8b36579a1a5b1a27c485c72ff8e8b873d7712
c.rabarijohn <c.rabarijohn@fulldigits.com>
2023-09-07 12:39:58 +0300

    Chore : Remove build folder and configure prod.yml

--- build/routes/admin.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// routes/authentication.js
-var express_1 = require("express");
-var router = (0, express_1.Router)();
-var authentification_controller_1 = require("../controller/authentification.controller");
-// Endpoint pour l'authentification (login)
-router.post('/login', authentification_controller_1.loginAdmin);
-exports.default = router;
--- build/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var router = (0, express_1.Router)();
-/* GET home page. */
-router.get('/', function (req, res) {
-    res.send('Karohy backend | |');
-});
-exports.default = router;
